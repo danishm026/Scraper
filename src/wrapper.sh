9c4d13e16dc03d0eb4d1d730a3b13f6d7814b3af
Danish <danishm026@gmail.com>
2016-04-07 16:32:22 +0530

    Modified wrapper.sh to get names of model from a file also

--- src/wrapper.sh
+++ src/wrapper.sh
@@ -3,14 +3,56 @@
 #activate virtual environment
 source ../env/bin/activate
 
-#Get Model Name 
-name=$1
+use_file=false
+while getopts "m:f:" opt
+do
+	case $opt in
+	m)
+		name=${OPTARG}
+		;;
+	f)
+		filename=${OPTARG}
+		use_file=true
+		;;
+	esac
+done
 
-#Run Script
-python scraper.py <<<$name
+if [ $use_file = false ]
+then
+	#Run Script
+	python scraper.py <<<$name
 
-filename=$( echo $name | sed -e "s/\b\(.\)/\u\1/g"| tr " " _)
-mkdir -p  $SCRAPER_DIRECTORY/$filename
-mv $filename $SCRAPER_DIRECTORY/$filename/
+	#Create filename to save links
+	filename=$( echo $name | sed -e "s/\b\(.\)/\u\1/g"| tr " " _)
+	
+	#Create directory to save images
+	mkdir -p  $SCRAPER_DIRECTORY/$filename
 
-wget --user-agent="" --continue --directory-prefix=$SCRAPER_DIRECTORY/$filename/ --input-file=$SCRAPER_DIRECTORY/$filename/$filename
+	#Move the links file to created directory
+	mv $filename $SCRAPER_DIRECTORY/$filename/
+
+	#Download the images in created directory
+	wget --user-agent="" --continue --directory-prefix=$SCRAPER_DIRECTORY/$filename/ --input-file=$SCRAPER_DIRECTORY/$filename/$filename
+
+else
+	while read name
+	do 
+		echo Processing $name
+		#Run Script
+		python scraper.py <<<$name
+		
+		#Create filename to save links
+		filename=$( echo $name | sed -e "s/\b\(.\)/\u\1/g"| tr " " _)
+		
+		#Create directory to save images
+		mkdir -p  $SCRAPER_DIRECTORY/$filename
+
+		#Move the links file to created directory
+		mv $filename $SCRAPER_DIRECTORY/$filename/
+
+		#Download the images in created directory
+		wget --user-agent="" --continue --directory-prefix=$SCRAPER_DIRECTORY/$filename/ --input-file=$SCRAPER_DIRECTORY/$filename/$filename
+
+
+	done < $filename
+fi
